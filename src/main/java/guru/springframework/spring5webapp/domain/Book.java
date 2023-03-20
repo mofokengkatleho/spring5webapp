5a201172ff2af069be2f43b4212c9e8a07b9b476
katleho <katlehomofokeng@protonmail.com>
2023-03-20 18:10:24 +0200

    POJOS

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,6 +1,43 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.Set;
+
 public class Book {
     private String title;
     private String isbn;
+    Set<Author> authorSet ;
+
+    public Book(){
+
+    }
+
+    public Book(String title, String isbn, Set<Author> authorSet) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authorSet = authorSet;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthorSet() {
+        return authorSet;
+    }
+
+    public void setAuthorSet(Set<Author> authorSet) {
+        this.authorSet = authorSet;
+    }
 }
