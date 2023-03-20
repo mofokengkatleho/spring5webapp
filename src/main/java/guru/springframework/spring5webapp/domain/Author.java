692f7eabb2b04cd507a44752615f726537dca456
katleho <katlehomofokeng@protonmail.com>
2023-03-20 18:13:12 +0200

    Entity author

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,8 +1,17 @@
 package guru.springframework.spring5webapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)//assigned by underlying database
+    private Long id;
+
     private String firstName;
     private String lastName;
     private Set<Book> books;
